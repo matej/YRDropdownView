02f4d15ceed3593085c1ad45c48d7f162024fcf6
Matej Bukovinski <matej@bukovinski.com>
2014-12-31 20:50:11 +0100

    Fixed status bar issues (mostly related to iOS 8).
    Unified spacing.

--- YRDropdownView/YRDropdownView.m
+++ YRDropdownView/YRDropdownView.m
@@ -25,10 +25,10 @@
 - (void)sizeToFitFixedWidth:(CGFloat)fixedWidth {
 	CGRect frame = self.frame;
 	frame.size.width = fixedWidth;
-    self.frame = frame;
-    self.lineBreakMode = NSLineBreakByWordWrapping;
-    self.numberOfLines = 0;
-    [self sizeToFit];
+	self.frame = frame;
+	self.lineBreakMode = NSLineBreakByWordWrapping;
+	self.numberOfLines = 0;
+	[self sizeToFit];
 }
 
 @end
@@ -42,6 +42,7 @@
 @property (nonatomic, weak) UIView *parentView;
 @property (nonatomic, assign) BOOL shouldAnimate;
 @property (nonatomic, assign) CGFloat dropdownHeight;
+@property (nonatomic, assign, readonly) CGFloat statusBarOffset;
 
 + (UIImageView *)imageViewWithImage:(UIImage *)image;
 
@@ -59,7 +60,7 @@ static BOOL __isQueuing = NO;
 
 + (void)toggleRtl:(BOOL)rtl
 {
-    __isRtl = rtl;
+	__isRtl = rtl;
 }
 
 + (void)toggleQueuing:(BOOL)queuing
@@ -79,8 +80,8 @@ static BOOL __isQueuing = NO;
 		_titleLabel.backgroundColor = [UIColor clearColor];
 		_titleLabel.textColor = [UIColor colorWithWhite:0.0f alpha:0.7f];
 		if (__isRtl) {
-            _titleLabel.textAlignment = self.detailLabel.textAlignment = NSTextAlignmentRight;
-        }
+			_titleLabel.textAlignment = self.detailLabel.textAlignment = NSTextAlignmentRight;
+		}
 		[self addSubview:_titleLabel];
 		[self setNeedsLayout];
 	}
@@ -98,8 +99,8 @@ static BOOL __isQueuing = NO;
 		_detailLabel.backgroundColor = [UIColor clearColor];
 		_detailLabel.textColor = [UIColor colorWithWhite:0.f alpha:0.5f];;
 		if (__isRtl) {
-            _detailLabel.textAlignment = self.detailLabel.textAlignment = NSTextAlignmentRight;
-        }
+			_detailLabel.textAlignment = self.detailLabel.textAlignment = NSTextAlignmentRight;
+		}
 		[self addSubview:_detailLabel];
 		[self setNeedsLayout];
 	}
@@ -120,27 +121,27 @@ static BOOL __isQueuing = NO;
 #pragma mark - Initializers
 - (id)init
 {
-    return [self initWithFrame:CGRectMake(0.0f, 0.0f, 320.0f, 44.0f)];
+	return [self initWithFrame:CGRectMake(0.0f, 0.0f, 320.0f, 44.0f)];
 }
 
 - (id)initWithFrame:(CGRect)frame
 {
-    self = [super initWithFrame:frame];
-    if (self) {
-        self.autoresizingMask = UIViewAutoresizingFlexibleWidth;
-        // Gentle shadow settings. Path will be set up live, in [layoutSubviews]
+	self = [super initWithFrame:frame];
+	if (self) {
+		self.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+		// Gentle shadow settings. Path will be set up live, in [layoutSubviews]
 		BOOL isRetina = [UIScreen mainScreen].scale >= 2;
 		// Hairline
-        self.layer.shadowOffset = CGSizeMake(0.f, isRetina ? 0.5f : 1.f);
-        self.layer.shadowRadius = 0.0f;
-        self.layer.shadowColor = [UIColor darkGrayColor].CGColor;
-        self.layer.shadowOpacity = 0.5f;
+		self.layer.shadowOffset = CGSizeMake(0.f, isRetina ? 0.5f : 1.f);
+		self.layer.shadowRadius = 0.0f;
+		self.layer.shadowColor = [UIColor darkGrayColor].CGColor;
+		self.layer.shadowOpacity = 0.5f;
 		self.backgroundColor = [UIColor colorWithRed:1.f green:.84f blue:0.16f alpha:0.95f];
 		// Hide on tap
 		UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(viewTapped:)];
 		[self addGestureRecognizer:tap];
-    }
-    return self;
+	}
+	return self;
 }
 
 #pragma mark - Class methods
@@ -157,7 +158,7 @@ static BOOL __isQueuing = NO;
 + (YRDropdownView *)dropdownInView:(UIView *)view title:(NSString *)title detail:(NSString *)detail accessoryView:(UIView *)accessoryView animated:(BOOL)animated
 {
 	YRDropdownView *dropdown = [[self alloc] initWithFrame:CGRectMake(0, 0, view.bounds.size.width, 44)];
-    
+
 	if ((__viewQueue && [__viewQueue count] > 0) || (__isQueuing && __currentDropdown)) {
 		if (!__viewQueue) __viewQueue = [NSMutableArray array];
 		[__viewQueue addObject:dropdown];
@@ -165,7 +166,7 @@ static BOOL __isQueuing = NO;
 		[__currentDropdown hide:__currentDropdown.shouldAnimate];
 		__currentDropdown = dropdown;
 	}
-	
+
 	if (title) {
 		dropdown.titleLabel.text = title;
 	}
@@ -175,10 +176,10 @@ static BOOL __isQueuing = NO;
 	if (accessoryView) {
 		dropdown.accessoryView = accessoryView;
 	}
-    
+
 	dropdown.shouldAnimate = animated;
 	dropdown.parentView = view;
-    
+
 	return dropdown;
 }
 
@@ -190,29 +191,29 @@ static BOOL __isQueuing = NO;
 
 + (BOOL)hideDropdownInView:(UIView *)view
 {
-    return [YRDropdownView hideDropdownInView:view animated:YES];
+	return [YRDropdownView hideDropdownInView:view animated:YES];
 }
 
 + (BOOL)hideDropdownInView:(UIView *)view animated:(BOOL)animated
 {
-    if (__currentDropdown) {
-        [__currentDropdown hide:animated];
-        return YES;
-    }
-    
-    UIView *viewToRemove = nil;
-    for (UIView *v in [view subviews]) {
-        if ([v isKindOfClass:[YRDropdownView class]]) {
-            viewToRemove = v;
-        }
-    }
-    if (viewToRemove != nil) {
-        YRDropdownView *dropdown = (YRDropdownView *)viewToRemove;
-        [dropdown hide:animated];
-        return YES;
-    } else {
-        return NO;
-    }
+	if (__currentDropdown) {
+		[__currentDropdown hide:animated];
+		return YES;
+	}
+
+	UIView *viewToRemove = nil;
+	for (UIView *v in [view subviews]) {
+		if ([v isKindOfClass:[YRDropdownView class]]) {
+			viewToRemove = v;
+		}
+	}
+	if (viewToRemove != nil) {
+		YRDropdownView *dropdown = (YRDropdownView *)viewToRemove;
+		[dropdown hide:animated];
+		return YES;
+	} else {
+		return NO;
+	}
 }
 
 + (void)presentDropdown:(YRDropdownView *)dropdownView
@@ -224,7 +225,7 @@ static BOOL __isQueuing = NO;
 		return;
 	}
 	__currentDropdown = dropdownView;
-	
+
 	[dropdownView.parentView addSubview:dropdownView];
 	[dropdownView show:dropdownView.shouldAnimate];
 	if (dropdownView.hideAfter != 0.0)
@@ -237,35 +238,35 @@ static BOOL __isQueuing = NO;
 
 + (BOOL)isCurrentlyShowing
 {
-    return __currentDropdown != nil;
+	return __currentDropdown != nil;
 }
 
 #pragma mark - Showing and Hiding
 
 - (void)show:(BOOL)animated
 {
-    if(animated)
-    {
-        UIInterfaceOrientation orientation = [[UIApplication sharedApplication] statusBarOrientation];
-        BOOL rotatedY = orientation == UIInterfaceOrientationPortraitUpsideDown && !self.isView;
-        int rotated = self.isView?0:(orientation == UIInterfaceOrientationLandscapeLeft ? 1 : (orientation == UIInterfaceOrientationLandscapeRight ? 2 : 0));
-        if (orientation != UIInterfaceOrientationPortrait) [self layoutSubviews];
-        CGRect originalRc = self.frame;
-        self.frame = CGRectMake(
-                                originalRc.origin.x+(rotated==1?-originalRc.size.width:(rotated==2?originalRc.size.width:0)),
-                                originalRc.origin.y+(rotated?0:(rotatedY?originalRc.size.height:-originalRc.size.height)),
-                                originalRc.size.width,
-                                originalRc.size.height);
-        self.alpha = 0;
-        
-        [UIView animateWithDuration:FADE_IN_ANIMATION_DURATION
-                              delay:0.0
-                            options:UIViewAnimationOptionCurveEaseInOut
-                         animations:^{
-                             self.alpha = 1.0;
-                         }
-                         completion:nil];
-		
+	if(animated)
+	{
+		UIInterfaceOrientation orientation = [[UIApplication sharedApplication] statusBarOrientation];
+		BOOL rotatedY = orientation == UIInterfaceOrientationPortraitUpsideDown && !self.hasViewCoordinates;
+		int rotated = self.hasViewCoordinates?0:(orientation == UIInterfaceOrientationLandscapeLeft ? 1 : (orientation == UIInterfaceOrientationLandscapeRight ? 2 : 0));
+		if (orientation != UIInterfaceOrientationPortrait) [self layoutSubviews];
+		CGRect originalRc = self.frame;
+		self.frame = CGRectMake(
+								originalRc.origin.x+(rotated==1?-originalRc.size.width:(rotated==2?originalRc.size.width:0)),
+								originalRc.origin.y+(rotated?0:(rotatedY?originalRc.size.height:-originalRc.size.height)),
+								originalRc.size.width,
+								originalRc.size.height);
+		self.alpha = 0;
+
+		[UIView animateWithDuration:FADE_IN_ANIMATION_DURATION
+							  delay:0.0
+							options:UIViewAnimationOptionCurveEaseInOut
+						 animations:^{
+							 self.alpha = 1.0;
+						 }
+						 completion:nil];
+
 		[UIView animateWithDuration:ANIMATION_DURATION
 							  delay:0.0
 			 usingSpringWithDamping:1.f
@@ -273,7 +274,7 @@ static BOOL __isQueuing = NO;
 							options:0 animations:^{
 								self.frame = originalRc;
 							} completion:nil];
-    }
+	}
 }
 
 - (void)hide:(BOOL)animated
@@ -283,20 +284,20 @@ static BOOL __isQueuing = NO;
 
 - (void)hideWithAnimation:(NSNumber *)animated
 {
-    if ([animated boolValue])
-    {
-        UIInterfaceOrientation orientation = [[UIApplication sharedApplication] statusBarOrientation];
-        BOOL rotatedY = orientation == UIInterfaceOrientationPortraitUpsideDown && !self.isView;
-        int rotated = self.isView ? 0 : (orientation == UIInterfaceOrientationLandscapeLeft ? 1 : (orientation == UIInterfaceOrientationLandscapeRight ? 2 : 0));
-        
+	if ([animated boolValue])
+	{
+		UIInterfaceOrientation orientation = [[UIApplication sharedApplication] statusBarOrientation];
+		BOOL rotatedY = orientation == UIInterfaceOrientationPortraitUpsideDown && !self.hasViewCoordinates;
+		int rotated = self.hasViewCoordinates ? 0 : (orientation == UIInterfaceOrientationLandscapeLeft ? 1 : (orientation == UIInterfaceOrientationLandscapeRight ? 2 : 0));
+
 		[UIView animateWithDuration:FADE_OUT_ANIMATION_DURATION
-                              delay:0.
-                            options:UIViewAnimationOptionCurveEaseInOut
-                         animations:^{
-                             self.alpha = 0.0;
-                         }
-                         completion:nil];
-		
+							  delay:0.
+							options:UIViewAnimationOptionCurveEaseInOut
+						 animations:^{
+							 self.alpha = 0.0;
+						 }
+						 completion:nil];
+
 		[UIView animateWithDuration:ANIMATION_DURATION
 							  delay:0.0
 			 usingSpringWithDamping:1.f
@@ -309,38 +310,38 @@ static BOOL __isQueuing = NO;
 							} completion:^(BOOL finished) {
 								[self done];
 							}];
-    }
-    else
-    {
-        self.alpha = 0.0f;
-        [self done];
-    }
+	}
+	else
+	{
+		self.alpha = 0.0f;
+		[self done];
+	}
 }
 
 - (void)done
 {
-    [self removeFromSuperview];
-    [[NSNotificationCenter defaultCenter] removeObserver:self];
-    
-    if (__viewQueue.count && __currentDropdown == self) // no need for nil check
-    {
-        __currentDropdown = __viewQueue[0];
-        [__viewQueue removeObjectAtIndex:0];
+	[self removeFromSuperview];
+	[[NSNotificationCenter defaultCenter] removeObserver:self];
+
+	if (__viewQueue.count && __currentDropdown == self) // no need for nil check
+	{
+		__currentDropdown = __viewQueue[0];
+		[__viewQueue removeObjectAtIndex:0];
 		[YRDropdownView presentDropdown:__currentDropdown];
-    }
-    else
-    {
-        __currentDropdown = nil;
-    }
+	}
+	else
+	{
+		__currentDropdown = nil;
+	}
 }
 
 #pragma mark - Gesures
 
 - (void)viewTapped:(UITapGestureRecognizer *)tapRecognizer {
 	if (self.tapBlock) {
-        dispatch_async(dispatch_get_main_queue(), self.tapBlock);
-    }
-    [self hide:self.shouldAnimate];
+		dispatch_async(dispatch_get_main_queue(), self.tapBlock);
+	}
+	[self hide:self.shouldAnimate];
 }
 
 #pragma mark - Layout
@@ -348,16 +349,18 @@ static BOOL __isQueuing = NO;
 - (void)layoutSubviews
 {
 	CGRect bounds = self.bounds;
+	bounds.origin.y += self.statusBarOffset;
+	bounds.size.height -= self.statusBarOffset;
 	CGRect availableBounds = CGRectInset(bounds, HORIZONTAL_PADDING, VERTICAL_PADDING);
-	
+
 	CGRect accessoryFrame = CGRectZero;
-    CGRect titleFrame = CGRectZero;
+	CGRect titleFrame = CGRectZero;
 	CGRect detailFrame = CGRectZero;
-	
+
 	BOOL hasAccessory = !!_accessoryView;
 	BOOL hasTitle = [_titleLabel.text length] > 0;
 	BOOL hasDetail = [_detailLabel.text length] > 0;
-	
+
 	if (hasAccessory) {
 		// Compute accesory frame
 		accessoryFrame = self.accessoryView.frame;
@@ -374,7 +377,7 @@ static BOOL __isQueuing = NO;
 			availableBounds.origin.x += accessoryFrame.size.width + HORIZONTAL_PADDING + ACCESSORY_PADDING;
 		}
 	}
-	
+
 	if (hasTitle) {
 		// Compute title frame
 		[_titleLabel sizeToFitFixedWidth:availableBounds.size.width];
@@ -388,11 +391,11 @@ static BOOL __isQueuing = NO;
 		}
 		// Adjust remaining available space
 		availableBounds.origin.y = CGRectGetMaxY(titleFrame);
-		availableBounds.size.height -= CGRectGetMaxY(titleFrame);
+		availableBounds.size.height -= CGRectGetHeight(titleFrame);
 	}
 	_titleLabel.frame = titleFrame;
-	
-	
+
+
 	if (hasDetail) {
 		// Compute detail frame
 		[_detailLabel sizeToFitFixedWidth:availableBounds.size.width];
@@ -406,60 +409,85 @@ static BOOL __isQueuing = NO;
 		}
 		// Adjust remaining available space
 		availableBounds.origin.y = CGRectGetMaxY(detailFrame);
-		availableBounds.size.height -= CGRectGetMaxY(detailFrame);
+		availableBounds.size.height -= CGRectGetHeight(detailFrame);
 	}
 	_detailLabel.frame = detailFrame;
-	
-    
+
+
 	CGFloat dropdownHeight = availableBounds.origin.y + VERTICAL_PADDING;
 	dropdownHeight = MAX(dropdownHeight, CGRectGetMaxY(_accessoryView.frame) + VERTICAL_PADDING);
 	self.dropdownHeight = dropdownHeight;
-    
+
 	UIInterfaceOrientation orientation = [[UIApplication sharedApplication] statusBarOrientation];
-	BOOL rotated = UIInterfaceOrientationIsLandscape(orientation) && !self.isView;
-    
+	BOOL rotated = UIInterfaceOrientationIsLandscape(orientation) && !self.hasViewCoordinates;
+
 	CGRect frame = self.frame;
 	[self setFrame:CGRectMake(frame.origin.x, frame.origin.y, rotated ? dropdownHeight : frame.size.width, rotated ? frame.size.height : dropdownHeight)];
-    
+
 	[self flipViewToOrientation:nil];
 }
 
 - (void)flipViewToOrientation:(NSNotification *)notification
 {
-    if (!__currentDropdown.isView) {
-        UIInterfaceOrientation orientation = [[UIApplication sharedApplication] statusBarOrientation];
-        
-        if (!self.dropdownHeight) return;
-        CGFloat angle = 0.0;
-        CGRect newFrame = self.window.bounds;
-        CGSize statusBarSize = [[UIApplication sharedApplication] statusBarFrame].size;
-        
-        switch (orientation) {
-            case UIInterfaceOrientationPortraitUpsideDown:
-                angle = M_PI;
-                newFrame.origin.y = newFrame.size.height - statusBarSize.height - self.dropdownHeight;
-                newFrame.size.height = self.dropdownHeight;
-                break;
-            case UIInterfaceOrientationLandscapeLeft:
-                angle = - M_PI / 2.0f;
-                newFrame.origin.x += statusBarSize.width;
-                newFrame.size.width = self.dropdownHeight;
-                break;
-            case UIInterfaceOrientationLandscapeRight:
-                angle = M_PI / 2.0f;
-                newFrame.origin.x = newFrame.size.width - statusBarSize.width - self.dropdownHeight;
-                newFrame.size.width = self.dropdownHeight;
-                break;
-            default: // as UIInterfaceOrientationPortrait
-                angle = 0.0;
-                newFrame.origin.y += statusBarSize.height;
-                newFrame.size.height = self.dropdownHeight;
-                newFrame.size.width = statusBarSize.width;
-                break;
-        }
-        self.transform = CGAffineTransformMakeRotation(angle);
-        self.frame = newFrame;
-    }
+	if (!__currentDropdown.hasViewCoordinates) {
+		UIInterfaceOrientation orientation = [[UIApplication sharedApplication] statusBarOrientation];
+
+		if (!self.dropdownHeight) return;
+		CGFloat angle = 0.0;
+		CGRect newFrame = self.window.bounds;
+		CGSize statusBarSize = [[UIApplication sharedApplication] statusBarFrame].size;
+
+		switch (orientation) {
+			case UIInterfaceOrientationPortraitUpsideDown:
+				angle = M_PI;
+				newFrame.origin.y = newFrame.size.height - self.dropdownHeight;
+				newFrame.size.height = self.dropdownHeight;
+				break;
+			case UIInterfaceOrientationLandscapeLeft:
+				angle = - M_PI / 2.0f;
+				newFrame.size.width = self.dropdownHeight;
+				break;
+			case UIInterfaceOrientationLandscapeRight:
+				angle = M_PI / 2.0f;
+				newFrame.origin.x = newFrame.size.width - self.dropdownHeight;
+				newFrame.size.width = self.dropdownHeight;
+				break;
+			default: // as UIInterfaceOrientationPortrait
+				angle = 0.0;
+				newFrame.size.height = self.dropdownHeight;
+				newFrame.size.width = statusBarSize.width;
+				break;
+		}
+		self.transform = CGAffineTransformMakeRotation(angle);
+		self.frame = newFrame;
+	}
+}
+
+- (CGFloat)statusBarOffset {
+	if (!__currentDropdown.isView) {
+		UIInterfaceOrientation orientation = [[UIApplication sharedApplication] statusBarOrientation];
+		CGSize statusBarSize = [[UIApplication sharedApplication] statusBarFrame].size;
+		if (self.hasViewCoordinates) {
+			return statusBarSize.height;
+		} else {
+			if (UIInterfaceOrientationIsPortrait(orientation)) {
+				return statusBarSize.height;
+			} else {
+				return statusBarSize.width;
+			}
+
+		}
+	}
+	return 0.f;
+}
+
+- (BOOL)hasViewCoordinates
+{
+	// On iOS 8+ UIWindow coordinates behave the same as UIView coordinates
+	if ([[[UIDevice currentDevice] systemVersion] compare:@"8.0" options:NSNumericSearch] == NSOrderedDescending) {
+		return YES;
+	}
+	return self.isView;
 }
 
 - (BOOL)isView
